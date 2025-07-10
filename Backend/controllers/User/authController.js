6a0a6add980620aefbbee41fd41f6e336c0a2e44
javid ibrahim <ibrupottayil@gmail.com>
2025-07-10 11:22:24 +0530

    update: cookie change

--- Backend/controllers/User/authController.js
+++ Backend/controllers/User/authController.js
@@ -81,12 +81,13 @@ exports.registerUser = errorHandling(async (req, res, next) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "Lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
     res.status(201).json({
         message: "User registered successfully",
@@ -122,12 +123,13 @@ exports.loginUser = errorHandling(async (req, res, next) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure: false,
-        sameSite: "Lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
     res.json({
         message: "Login successful",
@@ -193,12 +195,13 @@ exports.googleAuth = errorHandling(async (req, res, next) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
-    res.cookie("refreshToken", refreshToken, {
-        httpOnly: true,
-        secure:  process.env.NODE_ENV === "production",
-        sameSite: "Lax",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
+res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None", 
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
     console.log(req.cookies, "cookieeee");
     
 
